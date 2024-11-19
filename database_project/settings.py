37f9ca73fa3575bd66bf2b27700e89b58e7007f1
Atikarn KRUAYKRIANGKRAI <atikarn.kr@ku.th>
2024-11-20 05:15:54 +0700

    create appointments.html

--- database_project/settings.py
+++ database_project/settings.py
@@ -40,8 +40,6 @@ INSTALLED_APPS = [
     "paw_n_care",
     'django.contrib.humanize',
     'tailwind',
-    # 'theme',
-    # all-auth
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
@@ -129,6 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
