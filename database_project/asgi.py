31a71c8928d62b4973e6611fc25aa3ed84fcb6e5
Atikarn KRUAYKRIANGKRAI <atikarn.kr@ku.th>
2024-11-20 03:44:34 +0700

    setup project

--- database_project/asgi.py
+++ database_project/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for paw_n_care project.
+ASGI config for database_project project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'paw_n_care.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'database_project.settings')
 
 application = get_asgi_application()
---
+++ paw_n_care/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class PawNCareConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'paw_n_care'
